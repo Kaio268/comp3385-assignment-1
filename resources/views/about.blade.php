aa1603a66de1cf2fe9987947b29436f841347c54
Kaio268 <Makaio.Boodram@gmail.com>
2024-02-14 00:19:41 -0400

    Final adjustments and improvements

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>About Us</h2>
+    <p>This is a brief paragraph about us.</p>
+    <img src="{{ asset('images/setup.jpeg') }}" alt="Descriptive Alt Text">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController; // Add this line to use AboutController
 
 /*
 |--------------------------------------------------------------------------
@@ -8,8 +9,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -17,6 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function() { 
-    return "My About Page"; 
-     }); 
+// Update this route to use the AboutController@show method
+Route::get('/about', [AboutController::class, 'show']);
